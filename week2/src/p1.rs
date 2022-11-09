f0b6eebbff9d84cf0b4c5c71ac87c0702878c70c
Ashish <ashish.2017@hotmail.com>
2022-11-09 19:12:51 +0000

    fix:p1 - substring at end not counted

--- week2/src/p1.rs
+++ week2/src/p1.rs
@@ -1,29 +1,35 @@
 fn main() {
-	// have to do this as rust chars are unicode scalar values
-	let s1: Vec<char> = "abCd".chars().collect(); // O(n)
-	let s2: Vec<char> = "aaabCdabCdabCdabCdabCdabCdabCd".chars().collect(); // O(n)
+	substring::find_substrings("abCd", "aaabCdfffabCdsdkfabCd");
+}
 
-	let mut ptr = 0;
+pub mod substring {
+	pub fn find_substrings(s1: &str, s2: &str) {
+		// have to do this as rust chars are unicode scalar values
+		let s1: Vec<char> = s1.chars().collect(); // O(n)
+		let s2: Vec<char> = s2.chars().collect(); // O(n)
 
-	// O(n)
-	for (i, x) in s2.iter().enumerate() {
-		// O(1)
-		if ptr == s1.len() {
-			println!("{}", i - s1.len());
-			ptr = 0;
-		}
+		let mut ptr = 0;
 
-		// O(1)
-		if let Some(c) = s1.get(ptr) {
-			if c == x {
-				ptr += 1;
-			} else if *x == s1[0] {
-				ptr = 1;
-			} else {
+		// O(n)
+		for (i, x) in s2.iter().enumerate() {
+			let l = s1.len();
+
+			// O(1)
+			if ptr == l || (ptr == l - 1 && *x == s1[l - 1]) {
+				println!("{}", i - l);
 				ptr = 0;
+			}
+
+			// O(1)
+			if let Some(c) = s1.get(ptr) {
+				if c == x {
+					ptr += 1;
+				} else if *x == s1[0] {
+					ptr = 1;
+				} else {
+					ptr = 0;
+				}
 			}
 		}
 	}
-
-	// O(n) * O(1) = O(n)
 }
