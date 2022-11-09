c59da00623464c052a9472feab0af88bdb72aa05
Ashish <ashish.2017@hotmail.com>
2022-11-09 18:32:47 +0000

    corrected p1

--- week2/src/p1.rs
+++ week2/src/p1.rs
@@ -1,20 +1,29 @@
 fn main() {
+	// have to do this as rust chars are unicode scalar values
 	let s1: Vec<char> = "abCd".chars().collect(); // O(n)
-	let s2: Vec<char> = "ffffabCdCefg".chars().collect(); // O(n)
+	let s2: Vec<char> = "aaabCdabCdabCdabCdabCdabCdabCd".chars().collect(); // O(n)
 
 	let mut ptr = 0;
 
 	// O(n)
 	for (i, x) in s2.iter().enumerate() {
-		println!("ptr -> {}", ptr);
 		// O(1)
-		match s1.get(ptr) {
-			Some(c) if c == x => ptr += 1,
-			None => {
-				println!("{}", i - s1.len());
-				return;
+		if ptr == s1.len() {
+			println!("{}", i - s1.len());
+			ptr = 0;
+		}
+
+		// O(1)
+		if let Some(c) = s1.get(ptr) {
+			if c == x {
+				ptr += 1;
+			} else if *x == s1[0] {
+				ptr = 1;
+			} else {
+				ptr = 0;
 			}
-			_ => ptr = 0,
 		}
 	}
+
+	// O(n) * O(1) = O(n)
 }
